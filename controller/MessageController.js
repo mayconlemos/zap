e3fd1e72ad02c900ff53c9301362fddc242a20bf
unknown <lemos217@gmail.com>
2021-03-07 12:13:11 -0300

    modified:   controller/MessageController.js

--- controller/MessageController.js
+++ controller/MessageController.js
@@ -5,7 +5,10 @@ const fs = require('fs');
 
 var clientsArray = [];
 
-let chromiumArgs = ['--disable-web-security', '--no-sandbox', '--disable-web-security', '--aggressive-cache-discard', '--disable-cache', '--disable-application-cache', '--disable-offline-load-stale-cache', '--disk-cache-size=0', '--disable-background-networking', '--disable-default-apps', '--disable-extensions', '--disable-sync', '--disable-translate', '--hide-scrollbars', '--metrics-recording-only', '--mute-audio', '--no-first-run', '--safebrowsing-disable-auto-update', '--ignore-certificate-errors', '--ignore-ssl-errors', '--ignore-certificate-errors-spki-list'];
+let chromiumArgs = [ "--incognito",
+"--no-sandbox",
+"--single-process",
+"--no-zygote"];
 
 async function opendata(req, res, sessionName) {
     console.log(sessionName)
